96edac5a4373cb941697d93d8711f6d7448fe14f
Raymond Oluoch <rasholuoch@gmail.com>
2020-11-19 21:00:02 +0300

    adding linting configuration and env file example

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,13 +1,18 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
   extends: [
     'airbnb-base',
   ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
   },
   rules: {
   },
